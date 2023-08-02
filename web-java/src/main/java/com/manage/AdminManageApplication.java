c2bc14c13e8ba4eca8f1b589cb6bb63487f00c02
zhangbaiyoo <1781131728@qq.com>
2023-08-02 22:55:28 +0800

    web-java/web-vue

---
+++ web-java/src/main/java/com/manage/AdminManageApplication.java
@@ -0,0 +1,13 @@
+package com.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminManageApplication.class, args);
+    }
+
+}
