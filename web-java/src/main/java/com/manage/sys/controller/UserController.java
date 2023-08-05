2a65c55942c7c09edfd13c274a5a2ca651283af5
home <zhangqiushi0811@gmail.com>
2023-08-05 15:11:23 +0800

    新增登录接口

--- web-java/src/main/java/com/manage/sys/controller/UserController.java
+++ web-java/src/main/java/com/manage/sys/controller/UserController.java
@@ -4,15 +4,14 @@ import com.manage.VO.Result;
 import com.manage.sys.entity.User;
 import com.manage.sys.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author zhangBai
@@ -25,7 +24,14 @@ public class UserController {
     private IUserService userService;
 
     @GetMapping("/all")
-    public Result<List<User>> getAllUser(){
-        return Result.success("查询成功",userService.list());
+    public Result<List<User>> getAllUser() {
+        return Result.success("查询成功", userService.list());
+    }
+
+    @PostMapping("/login")
+    public Result<Map<String, Object>> login(@RequestBody User user) {
+        Map<String, Object> data = userService.login(user);
+        if (data != null) return Result.success(data);
+        return Result.Failed(200002, "用户名或密码错误");
     }
 }
--- web-java/src/main/java/com/manage/sys/service/impl/UserServiceImpl.java
+++ web-java/src/main/java/com/manage/sys/service/impl/UserServiceImpl.java
@@ -1,14 +1,22 @@
 package com.manage.sys.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.manage.sys.entity.User;
 import com.manage.sys.mapper.UserMapper;
 import com.manage.sys.service.IUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author zhangBai
@@ -17,4 +25,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Override
+    public Map<String, Object> login(User user) {
+        //根据用户查询
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(User::getUsername, user.getUsername());
+        wrapper.eq(User::getPassword, user.getPassword());
+        User loginUser = this.baseMapper.selectOne(wrapper);
+        //不为空 生成token 并将用户信息存入redis
+        if (loginUser != null) {
+            //暂时用UUID -> 最终jwt
+            String key = "user:" + UUID.randomUUID();
+            //存redis
+            loginUser.setPassword(null);
+            redisTemplate.opsForValue().set(key, loginUser, 24, TimeUnit.HOURS);
+            //返回数据
+            Map<String, Object> data = new HashMap<>();
+            data.put("token", key);
+            return data;
+        }
+
+        return null;
+    }
 }
